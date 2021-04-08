fcbc78e280b6d22920cf26efb2c992f7334c2961
sahanepic <sahanepic@gmail.com>
2021-04-08 08:41:48 +0530

    THe Printer

---
+++ src/main/java/com/sahanbcs/Utill/Printer.java
@@ -0,0 +1,8 @@
+package com.sahanbcs.Utill;
+
+public class Printer {
+
+    public static  void print( String string ){
+        System.out.println( string);
+    }
+}
---
+++ src/main/java/com/sahanbcs/randomnumber/SecureRandom.java
@@ -0,0 +1,11 @@
+package com.sahanbcs.randomnumber;
+
+import com.sahanbcs.Utill.Printer;
+
+public class SecureRandom {
+
+    public static void main(String[] args) {
+        Printer.print("Hello Printer");
+    }
+
+}
